68fe747c25be5339fd4998fd71cdbde90c001ba3
nielsswinkels <niels.sw@gmail.com>
2014-01-09 13:20:41 +0100

    list of courses

--- fika/views/courses.py
+++ fika/views/courses.py
@@ -2,6 +2,7 @@ from pyramid.view import view_config
 
 from fika.views.base import BaseView
 from fika.models.interfaces import ICourse
+from fika.models.interfaces import ICourses
 
 
 class CourseView(BaseView):
@@ -13,4 +14,9 @@ class CourseView(BaseView):
         for name in self.context.get_field_value('course_modules', ()):
             results.append(course_modules[name])
         self.response['course_modules'] = results
+        return self.response
+
+    @view_config(context = ICourses, renderer = "fika:templates/courses.pt")
+    def courses(self):
+        self.response['courses'] = self.context.values()
         return self.response
