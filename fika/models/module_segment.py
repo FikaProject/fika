d0dc6d18771cf6150934b973807ecee3c2e0169a
nielsswinkels <niels.sw@gmail.com>
2014-01-27 11:55:21 +0100

    Improved the layout of segments by removing meta stuff

--- fika/models/module_segment.py
+++ fika/models/module_segment.py
@@ -29,14 +29,23 @@ class TextSegment(ModuleSegment):
                'edit': 'TextSegmentSchema',
                'view': 'TextSegmentSchema'}
     
+    def render(self, request, view):
+        return u'<div class="segment">' + self.get_field_value('body', ()) + u'</div>'
+    
 @content_factory('ImageSegment')
 class ImageSegment(ModuleSegment):
     schemas = {'add': 'ImageSegmentSchema',
                'edit': 'ImageSegmentSchema',
                'view': 'ImageSegmentSchema'}
+    
+    def render(self, request, view):
+        return u'<div class="segment"><img class="image-segment" src="' + self.get_field_value('url', ()) + u'" /><div>' + self.get_field_value('description', ()) + u'</div></div>'
 
 @content_factory('YoutubeSegment')
 class YoutubeSegment(ModuleSegment):
     schemas = {'add': 'YoutubeSegmentSchema',
                'edit': 'YoutubeSegmentSchema',
                'view': 'YoutubeSegmentSchema'}
+    
+    def render(self, request, view):
+        return u'<div class="segment"><iframe width="560" height="315" src="//www.youtube.com/embed/' + self.get_field_value('youtube_link', ()) + u'" frameborder="0" allowfullscreen></iframe><div>' + self.get_field_value('description', ()) + u'</div></div>'
--- fika/views/course_modules.py
+++ fika/views/course_modules.py
@@ -21,12 +21,8 @@ class CourseModulesView(BaseView):
         self.response['used_in_courses'] = self.root['courses'].module_used_in(self.context.uid)
         return self.response
 
-    @view_config(context = ImageSegment, renderer = "fika:templates/image_segment.pt")
-    @view_config(context = YoutubeSegment, renderer = "fika:templates/youtube_segment.pt")
     @view_config(context = IModuleSegment, renderer = "fika:templates/form.pt")
     def module_segment(self):
-        #self.response['description'] = self.context.get_field_value('description', ())
-        #self.response['description'] = self.context.get_field_value('description', ())
         self.response['form'] = self.context.render(self.request, self)
         return self.response
     
