24c11f502280ac4e95754fc550d0b63c928464f3
nielsswinkels <niels.sw@gmail.com>
2014-02-25 14:32:09 +0100

    Fixing icons and layout of module and segment blocks

--- fika/static/common.js
+++ fika/static/common.js
@@ -1,6 +1,6 @@
 $( document ).ready(function(event) {
     $('.collapser').on('click', function(event) {
-        var target = $(this).children('.glyphicon');
+        var target = $(this).children('.glyphicon-chevron');
         target.toggleClass('glyphicon-chevron-up');
         target.toggleClass('glyphicon-chevron-down');
     });
