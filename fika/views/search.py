c094e53ab196c0bed8c57d00c09c49330bf4605f
robinharms <robin@betahaus.net>
2014-03-25 14:57:59 +0100

    Search cleanup

--- fika/views/search.py
+++ fika/views/search.py
@@ -12,13 +12,13 @@ class SearchView(BaseView):
     @view_config(context = ISiteRoot, name = "search", renderer = "fika:templates/search.pt")
     def search_view(self):
         query = self.request.GET.get('query', '')
-        found, docids = self.root.catalog.query("'%s' in searchable_text" % query)
         results = []
-        for docid in docids:
-            path = self.root.catalog.document_map.address_for_docid(docid)
-            obj = find_resource(self.root, path)
-            if self.context_has_permission(obj, security.VIEW):
-                results.append(obj)
-        self.response['found'] = found
+        if query:
+            docids = self.root.catalog.query("'%s' in searchable_text" % query)[1]
+            for docid in docids:
+                path = self.root.catalog.document_map.address_for_docid(docid)
+                obj = find_resource(self.root, path)
+                if self.context_has_permission(obj, security.VIEW):
+                    results.append(obj)
         self.response['results'] = results
         return self.response
