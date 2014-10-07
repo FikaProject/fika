c204ee54f0b2aba5b0984213fb97c3b345623daf
Jacob Michelsen <chromegadget@gmail.com>
2014-10-07 14:36:03 +0200

    Added some help text to course, course modules and segment creation forms.

--- fika/schemas/course_module.py
+++ fika/schemas/course_module.py
@@ -5,8 +5,10 @@ import deform
 
 
 class CourseModuleSchema(BaseSchema, DCMetadataSchema):
-    title = colander.SchemaNode(colander.String(),)
+    title = colander.SchemaNode(colander.String(),
+                                description = u"The title of the course module.")
     description = colander.SchemaNode(colander.String(),
+                                      description = u"A short description of the course module.",
                                       validator=colander.Length(max=140),
                                       widget=deform.widget.TextAreaWidget(rows=8, cols=40),
                                       missing = u"")
