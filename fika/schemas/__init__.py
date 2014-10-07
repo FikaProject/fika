1daf63550a78ec3121b10a62d643cc433196ab51
robinharms <robin@betahaus.net>
2014-10-07 09:47:04 +0200

    Removed unused file, missed this reference

--- fika/schemas/__init__.py
+++ fika/schemas/__init__.py
@@ -3,6 +3,5 @@
 def includeme(config):
     config.include('.course')
     config.include('.course_module')
-    config.include('.module_segment')
     config.include('.image_slideshow')
     config.include('.segment')
