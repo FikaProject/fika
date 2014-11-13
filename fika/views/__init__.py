73059c51bb2dee3a8f56d2bf0f74e0f606da0b96
Jacob Michelsen <chromegadget@gmail.com>
2014-11-13 17:31:04 +0100

    Updated for the new inline types

--- fika/views/__init__.py
+++ fika/views/__init__.py
@@ -5,8 +5,5 @@ def includeme(config):
     config.include('.my_courses')
     config.include('.courses')
     config.include('.pdf')
-    config.include('.image_slideshow')
-    config.include('.video')
-    config.include('.segment')
     config.include('.assessment_view')
     config.scan('fika.views')
--- fika/views/assessment_view.py
+++ fika/views/assessment_view.py
@@ -1,13 +1,11 @@
 from pyramid.view import view_config
-from pyramid.view import view_defaults
-from pyramid.httpexceptions import HTTPFound
 
 from arche import security
 from arche.views.base import BaseForm
 
 from fika.models.interfaces import IAssessment
 
-@view_config(name = 'inline', context = IAssessment, renderer = "fika:templates/assessment.pt",
+@view_config(name = 'inline_in_module', context = IAssessment, renderer = "fika:templates/assessment.pt",
               permission=security.PERM_VIEW)
 class AssessmentView(BaseForm):
 	type_name = u'Assessment'
