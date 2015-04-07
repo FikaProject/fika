ed7b7df4c8d71198cd617dfaaa60bd61648154d9
nielsswinkels <niels.sw@gmail.com>
2015-04-07 16:13:59 +0200

    add new permission Show controls

--- fika/__init__.py
+++ fika/__init__.py
@@ -1,6 +1,7 @@
 from pyramid.i18n import TranslationStringFactory
 
 _ = FikaTSF = TranslationStringFactory('fika')
+PERM_SHOW_CONTROLS = 'perm:Show controls'
 
 
 def includeme(config):
@@ -16,11 +17,15 @@ def includeme(config):
     from arche.security import get_acl_registry
     from arche.utils import get_content_factories
     from arche.security import ROLE_ADMIN
+    from arche.security import ROLE_EDITOR
     acl_reg = get_acl_registry(config.registry)
     factories = get_content_factories(config.registry)
     add_perms = []
     for factory in factories.values():
         if hasattr(factory, 'add_permission'):
             add_perms.append(factory.add_permission)
-    acl_reg.default.add(ROLE_ADMIN, add_perms)
+    #acl_reg.default.add(ROLE_ADMIN, add_perms)
+    acl_reg['public'].add(ROLE_ADMIN, add_perms)
+    
+    acl_reg['public'].add(ROLE_EDITOR, PERM_SHOW_CONTROLS)
     
