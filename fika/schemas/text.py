0f01d5f0ed82010ef6a21a6e54d6c4eee0f04724
nielsswinkels <niels.sw@gmail.com>
2015-04-10 16:08:28 +0200

    No description on text and imageslideshow

--- fika/schemas/text.py
+++ fika/schemas/text.py
@@ -5,11 +5,6 @@ import deform
 class TextSchema(colander.Schema):
     title = colander.SchemaNode(colander.String(),
                                 description=u"The title of the text.",)
-    description = colander.SchemaNode(colander.String(),
-                                      validator=colander.Length(max=140),
-                                      widget=deform.widget.TextAreaWidget(rows=8, cols=40),
-                                      missing = u"",
-                                      description=u"Write a short description of the text here. Max 140 characters.",)
     body = colander.SchemaNode(colander.String(),
                                widget=deform.widget.RichTextWidget(rows=8, cols=80),
                                description=u"Write the piece of text here.",)
