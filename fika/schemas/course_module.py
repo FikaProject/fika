f5a5d5dae5885c420272df35285f75dd6d12aa00
nielsswinkels <niels.sw@gmail.com>
2015-04-10 16:09:19 +0200

    No description on course module

--- fika/schemas/course_module.py
+++ fika/schemas/course_module.py
@@ -7,11 +7,6 @@ import deform
 class CourseModuleSchema(BaseSchema, DCMetadataSchema):
     title = colander.SchemaNode(colander.String(),
                                 description = u"The title of the course module.")
-    description = colander.SchemaNode(colander.String(),
-                                      description = u"A short description of the course module.",
-                                      validator=colander.Length(max=140),
-                                      widget=deform.widget.TextAreaWidget(rows=8, cols=40),
-                                      missing = u"")
 
 
 def includeme(config):
