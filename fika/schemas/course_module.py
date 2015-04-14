a8e01373baa81be433c0d59a69c4be88e4a8f1ed
nielsswinkels <niels.sw@gmail.com>
2015-04-14 11:50:04 +0200

    No description on module

--- fika/schemas/course_module.py
+++ fika/schemas/course_module.py
@@ -1,5 +1,7 @@
 from arche.schemas import BaseSchema
 from arche.schemas import DCMetadataSchema
+from arche.interfaces import ISchemaCreatedEvent
+
 import colander
 import deform
 
@@ -7,8 +9,13 @@ import deform
 class CourseModuleSchema(BaseSchema, DCMetadataSchema):
     title = colander.SchemaNode(colander.String(),
                                 description = u"The title of the course module.")
+    
+def course_module_schema_adjustment(schema, event):
+    if 'description' in schema:
+        del schema['description']
 
 
 def includeme(config):
     config.add_content_schema('CourseModule', CourseModuleSchema, 'add')
     config.add_content_schema('CourseModule', CourseModuleSchema, 'edit')
+    config.add_subscriber(course_module_schema_adjustment, [CourseModuleSchema, ISchemaCreatedEvent])
