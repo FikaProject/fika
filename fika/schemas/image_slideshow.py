3513e96220493db5e34ab4fa31311a53d2f14d51
nielsswinkels <niels.sw@gmail.com>
2015-04-14 12:08:47 +0200

    No title on images

--- fika/schemas/image_slideshow.py
+++ fika/schemas/image_slideshow.py
@@ -1,11 +1,24 @@
 import colander
 import deform
 
+from arche.schemas import AddFileSchema
+from arche.interfaces import ISchemaCreatedEvent
+
 
 class ImageSlideshowSchema(colander.Schema):
     title = colander.SchemaNode(colander.String(),)
+    
+class AddImageSchema(AddFileSchema):
+    pass
+
+def image_schema_adjustment(schema, event):
+    if 'title' in schema:
+        del schema['title']
 
 
 def includeme(config):
     config.add_content_schema('ImageSlideshow', ImageSlideshowSchema, 'add')
     config.add_content_schema('ImageSlideshow', ImageSlideshowSchema, 'edit')
+    config.add_content_schema('Image', AddImageSchema, 'add') #Specific schema?
+    config.add_content_schema('Image', AddImageSchema, 'edit')
+    config.add_subscriber(image_schema_adjustment, [AddImageSchema, ISchemaCreatedEvent])
