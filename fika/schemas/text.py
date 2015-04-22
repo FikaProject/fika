d76cd81b4d6a3560964196016bf5614fbda785b0
nielsswinkels <niels.sw@gmail.com>
2015-04-22 13:05:44 +0200

    bigger textarea when adding text

--- fika/schemas/text.py
+++ fika/schemas/text.py
@@ -7,7 +7,7 @@ class TextSchema(colander.Schema):
     title = colander.SchemaNode(colander.String(),
                                 description=_(u"The title of the text."),)
     body = colander.SchemaNode(colander.String(),
-                               widget=deform.widget.RichTextWidget(rows=8, cols=80),
+                               widget=deform.widget.RichTextWidget(rows=24, cols=80),
                                description=_(u"Write the piece of text here."),)
 
 
